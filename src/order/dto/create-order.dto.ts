d99f641ec227be47e697780d175a04124e814688
Inam Ullah Yousufzai <58142833+inamsaif0@users.noreply.github.com>
2024-05-22 14:49:00 +0500

    Update create-order.dto.ts
    dto has changed

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,87 +1,125 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsArray, ValidateNested, IsString, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
+
+class LineItemDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  price: number;
+
+
+  @IsNotEmpty()
+  @IsNumber()
+  quantity: number;
+}
+
+class TransactionDto {
+  @IsNotEmpty()
+  @IsString()
+  kind: string;
+
+  @IsNotEmpty()
+  @IsString()
+  status: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  amount: number;
+}
 
 export class CreateOrderDto {
-  @IsNotEmpty()
-  line_items_title: string;
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => LineItemDto)
+  line_items: LineItemDto[];
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => TransactionDto)
+  transactions: TransactionDto[];
 
   @IsNotEmpty()
-  line_items_price: number;
-
-  @IsNotEmpty()
-  line_items_grams: number;
-
-  @IsNotEmpty()
-  line_items_quantity: number;
-
-  @IsNotEmpty()
-  transactions_kind: string;
-
-  @IsNotEmpty()
-  transactions_status: string;
-
-  @IsNotEmpty()
-  transactions_amount: number;
-
-  @IsNotEmpty()
+  @IsString()
   currency: string;
 
   @IsNotEmpty()
+  @IsString()
   billing_address_first_name: string;
 
   @IsNotEmpty()
+  @IsString()
   billing_address_last_name: string;
 
   @IsNotEmpty()
+  @IsString()
   billing_address_address1: string;
 
-  @IsNotEmpty()
+  @IsString()
   billing_address_address2: string;
 
   @IsNotEmpty()
+  @IsString()
   billing_address_city: string;
 
   @IsNotEmpty()
+  @IsString()
   billing_address_province: string;
 
   @IsNotEmpty()
+  @IsString()
   billing_address_country: string;
 
   @IsNotEmpty()
+  @IsString()
   billing_address_zip: string;
 
   @IsNotEmpty()
+  @IsString()
   shipping_address_first_name: string;
 
   @IsNotEmpty()
+  @IsString()
   shipping_address_last_name: string;
 
   @IsNotEmpty()
+  @IsString()
   shipping_address_address1: string;
 
-  @IsNotEmpty()
+  @IsString()
   shipping_address_address2: string;
 
   @IsNotEmpty()
+  @IsString()
   shipping_address_city: string;
 
   @IsNotEmpty()
+  @IsString()
   shipping_address_province: string;
 
   @IsNotEmpty()
+  @IsString()
   shipping_address_country: string;
 
   @IsNotEmpty()
+  @IsString()
   shipping_address_zip: string;
 
   @IsNotEmpty()
+  @IsString()
   customer_first_name: string;
 
   @IsNotEmpty()
+  @IsString()
   customer_last_name: string;
 
   @IsNotEmpty()
+  @IsString()
   customer_email: string;
 
   @IsNotEmpty()
+  @IsString()
   customer_phone: string;
 }
