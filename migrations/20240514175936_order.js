abe42762dfe7a2ec7848a13fed588db34cc20d61
Inam Ullah Yousufzai <58142833+inamsaif0@users.noreply.github.com>
2024-05-23 12:29:09 +0500

    Update 20240514175936_order.js
    dto

--- migrations/20240514175936_order.js
+++ migrations/20240514175936_order.js
@@ -8,6 +8,7 @@ exports.up = function (knex) {
             table.increments('id');
             table.string('order_id', 255).nullable();
             table.string('user_id', 255).nullable();
+            table.string('device_token', 255).nullable();
             table.string('amount', 255).nullable();
             table.timestamps(true, true);
         })
