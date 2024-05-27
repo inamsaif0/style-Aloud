d8597f34f99a6a8ee883b3a6b4ebcf18f7aad36a
inamsaif0 <inamsaif586@gmail.com>
2024-05-27 11:28:08 -0700

    thank you message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Get('/hello-world')
+  @Get('/thanks')
   getHello () {
     return this.appService.getHello();
   }
